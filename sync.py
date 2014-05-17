e0c066586e465724d1f37e18bfb24a127026d7d9
Anders Bergh <anders1@gmail.com>
2014-05-17 06:19:19 +0000

    sync.py: Add constants.

--- sync.py
+++ sync.py
@@ -6,10 +6,20 @@ import re
 import subprocess
 from glob import glob
 
+
+SCRIPTS_URL = 'http://portal.cloud1.codebust.com/u/anders/scripts.lua?json'
+
+# git settings
+GIT = 'git'
+AUTHOR_NAME = 'L. Bot <luabot@codebust.com>'
+BRANCH_NAME = 'master'
+REMOTE_NAME = 'origin'
+
+
 def main():
   root = os.path.dirname(os.path.realpath(__file__))
 
-  req = urllib.urlopen('http://portal.cloud1.codebust.com/u/anders/scripts.lua?json')
+  req = urllib.urlopen(SCRIPTS_URL)
   resp = req.read()
   req.close()
 
@@ -19,7 +29,7 @@ def main():
   lastrun = 0
 
   try:
-    p = subprocess.Popen(['git', 'log', '-1', 'master', '--format=%ct', '--grep', 'Sync.'], stdout=subprocess.PIPE)
+    p = subprocess.Popen([GIT, 'log', '-1', 'master', '--format=%ct', '--grep', 'Sync.'], stdout=subprocess.PIPE)
     lastrun = int(p.communicate()[0])
   except:
     pass
@@ -44,7 +54,7 @@ def main():
         with open(path, 'w') as f:
           f.write(resp)
         
-        subprocess.check_call(['git', 'add', path], stdout=fnull)
+        subprocess.check_call([GIT, 'add', path], stdout=fnull)
 
   # Find out if a script was deleted or not.
   # Done by finding Lua scripts that weren't in the list fetched earlier.
@@ -53,10 +63,10 @@ def main():
     for path in glob(os.path.join(root, mod) + '/*.lua'):
       name = os.path.splitext(os.path.basename(path))[0]
       if not name in j[mod]:
-        subprocess.check_call(['git', 'rm', path], stdout=fnull)
+        subprocess.check_call([GIT, 'rm', path], stdout=fnull)
 
-  subprocess.call(['git', 'commit', '--author', 'L. Bot <luabot@codebust.com>', '-m', 'Sync.'], stdout=fnull, stderr=fnull)
-  subprocess.call(['git', 'push', '-u', 'origin', 'master'], stdout=fnull, stderr=fnull)
+  subprocess.call([GIT, 'commit', '--author', AUTHOR_NAME, '-m', 'Sync.'], stdout=fnull, stderr=fnull)
+  subprocess.call([GIT, 'push', '-u', REMOTE_NAME, BRANCH_NAME], stdout=fnull, stderr=fnull)
 
   fnull.close()
 
