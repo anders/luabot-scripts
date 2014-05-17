da2dcdae571373df26420cbfdc4e4e69894a6e9b
Anders Bergh <anders1@gmail.com>
2014-05-17 04:36:51 +0000

    sync.py: Script to download and track luabot scripts in a git repo.

---
+++ sync.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python2.7
+import os
+import urllib
+import json
+import re
+import subprocess
+
+def main():
+  req = urllib.urlopen('http://portal.cloud1.codebust.com/u/anders/scripts.lua?json')
+  resp = req.read()
+  req.close()
+
+  j = json.loads(resp)
+
+  root = os.path.dirname(os.path.realpath(__file__))
+
+  for mod in j:
+    assert re.match(r'^[a-z]*$', mod)
+    
+    if not os.path.isdir(mod):
+      os.mkdir(mod)
+    
+    for fun in j[mod]:
+      assert re.match(r'^[a-zA-Z0-9_]+$', fun)
+      
+      path = os.path.join(root, mod, fun + '.lua')
+      
+      if not os.path.exists(path) or os.path.getmtime(path) < j[mod][fun]['mtime']:
+        print 'getting script %s.%s' % (mod, fun)
+        req = urllib.urlopen(j[mod][fun]['url'])
+        resp = req.read()
+        with open(path, 'w') as f:
+          f.write(resp)
+        os.utime(path, (-1, j[mod][fun]['mtime']))
+        
+        subprocess.call(['git', 'add', path])
+
+  print 'committing'
+  subprocess.call(['git', 'commit', '--author', 'L. Bot <luabot@codebust.com>', '-m', 'Sync.'])
+   
+
+if __name__ == '__main__':
+  main()
