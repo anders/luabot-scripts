7789739821358d4f4aabaa7d7f19f00cd0236f6f
Anders Bergh <anders1@gmail.com>
2015-01-09 13:36:44 +0100

    Retrieve names and emails from luabot

--- sync.py
+++ sync.py
@@ -8,6 +8,7 @@ from glob import glob
 
 
 SCRIPTS_URL = "http://portal.cloud1.codebust.com/u/anders/scripts.lua?json"
+MAP_URL = "http://portal.cloud1.codebust.com/u/anders/web_useremails.lua"
 
 # git settings
 GIT = "git"
@@ -17,9 +18,7 @@ BRANCH_NAME = "master"
 REMOTE_NAME = "origin"
 
 
-EMAIL_MAP = {
-    "anders": "anders1@gmail.com",
-}
+USER_MAP = {}
 
 
 def extend_list(a, b):
@@ -31,6 +30,13 @@ def main():
     script_path = os.path.realpath(__file__)
 
     root = os.path.dirname(script_path)
+
+    # retrieve user->name, email mapping
+    req = urllib.urlopen(MAP_URL)
+    resp = req.read()
+    req.close()
+
+    USER_MAP = json.loads(resp)
 
     req = urllib.urlopen(SCRIPTS_URL)
     resp = req.read()
@@ -95,12 +101,11 @@ def main():
             subprocess.check_call([GIT, "add", path], stdout=fnull)
 
         # opt-in author, otherwise semi-anonymous
-        name = user
-        if user not in EMAIL_MAP:
-            name = "user%d" % path_uid[path]
+        userinfo = USER_MAP.get(user, {})
 
-        os.environ["GIT_AUTHOR_NAME"] = name
-        os.environ["GIT_AUTHOR_EMAIL"] = EMAIL_MAP.get(user, "user%d@codebust.com" % path_uid[path])
+        os.environ["GIT_AUTHOR_NAME"] = userinfo.get("name", "user%d" % path_uid[path])
+        os.environ["GIT_AUTHOR_EMAIL"] = userinfo.get("email", "user%d@codebust.com" % path_uid[path])
+
         subprocess.check_call(extend_list([GIT, "commit", "-m", "Sync."], scripts_by_user[user]), stdout=fnull)
 
     os.environ["GIT_AUTHOR_NAME"] = AUTHOR_NAME
