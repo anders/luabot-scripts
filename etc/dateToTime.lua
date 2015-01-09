b665eecdec76defae44ae8423b3b585fd80695c6
user1 <user1@codebust.com>
2015-01-09 12:19:56 +0000

    Sync.

--- etc/dateToTime.lua
+++ etc/dateToTime.lua
@@ -1,6 +1,6 @@
 API "1.1"
 
--- Usage: etc.dateToTime(year-month-day)
+-- Usage: etc.dateToTime(year-month-day) - timestamp of the start of the provided day
 
 local y, m, d = assert(arg[1]):match("^(%d%d%d%d)%-(%d%d)%-(%d%d)$")
 if y then
