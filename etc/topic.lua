4542833833ae572b6f6669142cfc4489c0837901
Bug Maker <user1@codebust.com>
2015-01-10 04:48:03 +0000

    Sync.

--- etc/topic.lua
+++ etc/topic.lua
@@ -1,4 +1,4 @@
--- Usage: 'topic to get or append to the topic, include -s to set without append.
+-- Usage: 'topic to get or append to the topic, use -s to set without append. Other switches: -r [<n>] to remove a part, -c to clear, -u to undo.
 
 if not arg[1] then
   return topic(dest) or ''
@@ -10,16 +10,34 @@ Cache.topicid = _threadid
 local allowChans = { ["#dbot"] = true }
 
 if allowChans[dest] then
-  local newtopic = arg[1]
-  if newtopic:find("^%-s ") then
-    newtopic = newtopic:sub(4)
+  local oldtopic = topic(dest) or ''
+  local opt, after = arg[1]:match("^%-(.) ?(.*)$")
+  local newtopic = after or arg[1]
+  if opt == "s" then
+    -- Set the entire topic.
+  elseif opt == "r" then
+    -- Remove a part of the topic, defaults to 1 (1 appears first, the last added)
+    local nr = tonumber(after) or 1
+    local tparts = etc.splitByPattern(oldtopic, " %| ")
+    table.remove(tparts, nr)
+    newtopic = table.concat(tparts, " | ")
+  elseif opt == "u" then
+    -- Topic undo.
+    newtopic = assert(LocalCache.oldtopic, "No topic")
+    LocalCache.oldtopic = nil
+  elseif opt == "c" then
+    -- Topic clear.
+    newtopic = ''
   else
-    local prev = topic(dest)
-    if prev then
-      newtopic = newtopic .. " | " .. prev
+    assert(opt == nil, "What is -" .. (opt or "?"))
+    if oldtopic ~= '' then
+      newtopic = newtopic .. " | " .. oldtopic
     end
   end
   assert(_setTopic(newtopic))
+  if oldtopic ~= '' then
+    LocalCache.oldtopic = oldtopic
+  end
   singleprint()
   return dest, "topic set"
 else
