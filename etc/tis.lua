7ff14edd9d5ebd7b19278fa51be0778d0cabc793
user66 <user66@codebust.com>
2015-01-10 12:48:05 +0000

    Sync.

--- etc/tis.lua
+++ etc/tis.lua
@@ -1,3 +1,10 @@
 API "1.1"
 
-return "'tis really not " .. arg[1]
+local ADJS = {"totally", "really", "absolutely", "completely", "tubularly", "entirely", "wholly", "thoroughly", "utterly", "perfectly", "quite", "unconditionally", "altogether"}
+local adjs = {}
+
+for i=1,math.floor(math.random() * 10) do
+  adjs[#adjs + 1] = pickone(ADJS)
+end
+
+return "'tis " .. table.concat(adjs, ", ") .. " not " .. arg[1]
