a155df6bd6cb2f8634a2cb47de84dff145cc2ea6
Bug Maker <user1@codebust.com>
2015-01-28 01:48:06 +0000

    Sync.

---
+++ etc/sofunny.lua
@@ -0,0 +1,7 @@
+API "1.1"
+-- Usage: just 'sofunny something and it'll be funny.
+
+print(etc.getOutput(etc.tr, ...))
+print(etc.getOutput(etc.er, ...))
+print(etc.getOutput(etc.xxx, ...))
+print(etc.getOutput(etc.spooner, ...))
