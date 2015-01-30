2fb2444b0501dd722f52d8ef5eff40a89fe0b20a
Bug Maker <user1@codebust.com>
2015-01-30 09:48:08 +0000

    Sync.

--- etc/choose.lua
+++ etc/choose.lua
@@ -1,15 +1,15 @@
 API "1.1"
 -- Usage: 'choose this or that
 
-local a, b = (arg[1] or ''):match("^(.+) [oO][rR] (.+)$")
-if b then
-  if math.random(1, 2) == 1 then
-    return a
-  else
-    return b
-  end
-elseif arg[1] then
-  return pickone(etc.splitByPattern(arg[1], "[ ,%.!%?%|]+"))
-else
+if not arg[1] or arg[1] == "" then
   return false, 'What or what?'
 end
+
+local splitter = "[ ,%.!%?%|]+"
+if arg[1]:find("%|") then
+  splitter = " *%|+ *"
+elseif arg[1]:find(" [oO][rR] ") then
+  splitter = " +[oO][rR] +"
+end
+
+return pickone(etc.splitByPattern(arg[1], splitter))
