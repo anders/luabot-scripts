d1a26b7f054dd9dfb06aa7b37ccd02484273142d
Anders Bergh <anders1@gmail.com>
2015-02-03 17:48:06 +0000

    Sync.

--- etc/stutter.lua
+++ etc/stutter.lua
@@ -1,10 +1,12 @@
 local s_senpai = 0.65
-local text, always_stutter = ...
+local text = ...
 local rand = math.random
 
 return (arg[1]:gsub("(%a[%w%p]+)", function(w)
-  if always_stutter or rand() >= s_senpai then
-    return (w:sub(1, 1).."-"):rep(rand(1, 2) + rand(0, 1))..w
+  if rand() >= s_senpai then
+    --return (w:sub(1, 1).."-"):rep(rand(1, 2) + rand(0, 1))..w
+    local c = w:sub(1, 1)
+    return c:rep(rand(1, rand() >= 0.7 and 3 or 1)).."-"..(c.."-"):rep(rand(0, 2))..w
   else
     return w
   end
