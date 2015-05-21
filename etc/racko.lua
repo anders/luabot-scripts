da1b5e659e386dadb7cb338d2b264cf39c4df93a
Anders Bergh <anders1@gmail.com>
2015-05-21 04:48:07 +0000

    Sync.

--- etc/racko.lua
+++ etc/racko.lua
@@ -1,6 +1,10 @@
 API "1.1"
 
+if not chan then return end
+
 local json = require "json"
+
+local Log = plugin.log("Rack-O")
 
 --[[
 Rack-O
@@ -31,6 +35,54 @@ discarded. if it was taken from the discard pile, it must be put in the rack.
 first player to get an ascending sequence of 10 cards wins
 ]]
 
+local SAVE_DIR = "racko"
+local STALE_TIME = 3600
+local MIN_PLAYERS = 2
+local MAX_PLAYERS = 4
+
+local function clean_slate()
+  return {
+    last_activity = os.time(),
+    players = {}
+  }
+end
+
+local function load_game()
+  Log.trace("load_game()")
+  local state
+
+  os.mkdir(SAVE_DIR)
+
+  local game_path = SAVE_DIR.."/"..chan..".dat"
+  local f = io.open(game_path, "r")
+  local data
+  if f then
+    data = f:read("*a")
+    f:close()
+    state = json.decode(data)
+  end
+  
+  if not state or (state.last_activity and os.time() - state.last_activity > STALE_TIME) then
+    return clean_slate()
+  else
+    return state
+  end
+end
+
+local function save_game(state)
+  -- only save if requested
+  Log.trace("save_game(): dirty="..tostring(state.dirty))
+  if state.dirty then
+    state.dirty = nil
+    local game_path = SAVE_DIR.."/"..chan..".dat"
+    local f = io.open(game_path, "w")
+    Log.trace("save_game(): "..game_path..": "..tostring(f))
+    local serialized = json.encode(state)
+    f:write(serialized)
+    f:close()
+  end
+end
+
 local function is_sequence(rack)
   for i=1, #rack do
     if rack[1] + i - 1 ~= rack[i] then
@@ -47,6 +99,27 @@ local function is_racko(rack)
   return true
 end
 
+local function count_players(state)
+  local n = 0
+  for k, v in pairs(state.players) do n = n + 1 end
+  return n
+end
+
+local function add_player(state, id)
+  if state.players[id] then
+    return false, "You are already in the game."
+  end
+
+  if count_players(state) >= MAX_PLAYERS then
+    return false, "Maximum number of players is "..MAX_PLAYERS.."."
+  end
+  
+  state.players[id] = {}
+  state.dirty = true
+  
+  return count_players(state)
+end
+
 local function msg_handler(state, reply, name, id, line)
   line = line or ""
 
@@ -54,8 +127,6 @@ local function msg_handler(state, reply, name, id, line)
   for token in line:gmatch("[^%s]+") do
     tokens[#tokens + 1] = token
   end
-
-  local has_joined = state and state.players[id] ~= nil
 
   if tokens[1] == "" or tokens[1] == "new" then
     if state then
@@ -70,21 +141,20 @@ local function msg_handler(state, reply, name, id, line)
     end
 
   elseif tokens[1] == "join" then
-    local ok, err = add_player(state, id)
-    if not ok then
+    local player_count, err = add_player(state, id)
+    if not player_count then
       reply("Sorry: "..err)
       return
     end
 
-    local n = count_players(state)
-    if n >= 2 and n <= 4 then
-      reply("You have joined the game. There are enough players to play, you can use 'racko start")
+    if player_count >= MIN_PLAYERS then
+      reply("You have joined the game. There are enough players to begin, you can use 'racko start")
     else
       reply("You have joined the game, need at least one more player to start.")
     end
 
   elseif tokens[1] == "racko" then
-    if not has_joined then
+    if not state.players[id] then
       reply("You haven't joined the game")
       return
     end
@@ -106,10 +176,14 @@ local function make_reply_func(nick)
   end
 end
 
-if not account or account < 1 or account > 1000000 then
+-- 28292717 = $guest
+if not account or account < 1 or account >= 2829271723423 then
   print(nick.." * you need to be authenticated to play.")
   return
 end
 
--- 3rd arg should be the user id
+local state = load_game()
+
 msg_handler(state, make_reply_func(nick), nick, account, arg[1])
+
+save_game(state)
