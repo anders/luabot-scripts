ae3fa0152c9bccbd205f68bb1a533fc9663213bd
user66 <user66@codebust.com>
2015-07-23 19:48:06 +0000

    Sync.

--- etc/censor.lua
+++ etc/censor.lua
@@ -1,3 +1,3 @@
 API "1.1"
 
-return arg[1]:gsub("([aeiouAEIOU]+)", function (c) return math.random() <= 0.5 and string.rep("*", #c) or c end)
+return arg[1]:gsub("([aeiouAEIOU]+)", function (c) return math.random() <= 0.5 and string.rep("*", #c) or c end) or ""
