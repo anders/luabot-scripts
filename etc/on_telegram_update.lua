97a1d8729246b73e7c5bb2a72dc258ee0da242f3
Bug Maker <user1@codebust.com>
2015-08-31 00:48:06 +0000

    Sync.

--- etc/on_telegram_update.lua
+++ etc/on_telegram_update.lua
@@ -1,6 +1,34 @@
 API "1.1"
 
-local response = Telegram.update
+local T = Telegram
+Telegram = nil
 
-_clown()
-print("telegram: ", response)
+-- _clown()
+-- print("telegram: ", response)
+
+if not T.update or T.update == "" then
+  return
+end
+
+local json = require("json")
+
+local rsp = json.decode(T.update)
+
+if rsp.ok and type(rsp.result) == "table" then
+  for i = 1, #rsp.result do
+    local r = rsp.result[i]
+    if r.message then
+      if r.message.chat then
+        local chat = r.message.chat -- id, first_name, last_name, username
+        local from = r.message.from -- id, first_name, last_name, username
+        local msg = r.message.text
+        _clown()
+        print("Telegram from " .. from.first_name .. ": " .. msg)
+      else
+        -- got non-chat message
+      end
+    else
+      -- got non-message
+    end
+  end
+end
