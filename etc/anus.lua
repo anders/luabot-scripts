2c685321908cf89a97fb9486fcd5b0e4b598fc3d
Anders Bergh <anders1@gmail.com>
2016-01-23 00:48:07 +0000

    Sync.

--- etc/anus.lua
+++ etc/anus.lua
@@ -1,6 +1,15 @@
-local threshold = 0.9
-return etc.translateWords(arg[1], function(w)
-  if math.random() > threshold then
-    return "anus"
-  end
-end)
+local function x(arg)
+  local threshold = 0.9
+  return etc.translateWords(arg[1], function(w)
+    if math.random() > threshold then
+      return "anus"
+    end
+  end)
+end
+
+local y
+repeat
+  y = x(arg)
+until y:find("anus")
+
+return y
