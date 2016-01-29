20f6e7384d04ce07c6044ba593aa38726ca2ee12
Anders Bergh <anders1@gmail.com>
2016-01-29 14:48:05 +0000

    Sync.

--- etc/boypussy.lua
+++ etc/boypussy.lua
@@ -1,5 +1,9 @@
 API "1.1"
 
+if nick:find('gmcabrita') then
+  return '<'..pickone{'graphitemastercabrita', 'gaymancabrita'}..'> meme'
+end
+
 return pickone({
 function()
   return '<supergauntlet> dude '..pickone{rnick(), 'weed'}..' lmao'
