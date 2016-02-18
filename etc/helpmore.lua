4331ba03054c17927c2b3e59e447a728f130c554
Bug Maker <user1@codebust.com>
2016-02-18 09:48:04 +0000

    Sync.

---
+++ etc/helpmore.lua
@@ -0,0 +1,26 @@
+API "1.1"
+
+local WANT = 15
+local t = etc.find("*", true)
+local nfound = 0
+local maxtries = 20
+for i = 1, maxtries do
+  if nfound < WANT then
+    local r = math.random(#t)
+    local isbad = t[r]:find("^on_") or t[r]:find("^cron_")
+    local code = getCode('etc', t[r])
+    local hasusage = code:find("Usage", 1, true)
+    local foolish = code:find("[Dd]eprecated") or code:find("[Ee]xperiment")
+    local desperate = i >= ((maxtries - 7) - nfound)
+    local lonely = i >= (maxtries - nfound)
+    if lonely or (not isbad and hasusage and (not foolish or desperate)) then
+      nfound = nfound + 1
+      t[nfound], t[r] = t[r], t[nfound]
+    end
+  end
+end
+local s = ""
+for i = 1, WANT do
+  s = s .. " " .. etc.cmdprefix .. t[i]
+end
+return s
