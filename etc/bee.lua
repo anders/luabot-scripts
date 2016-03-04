3484ba500a44cd1687b931f3a6d59e03ef501a4d
Anders Bergh <anders1@gmail.com>
2016-03-04 16:48:03 +0000

    Sync.

--- etc/bee.lua
+++ etc/bee.lua
@@ -1,9 +1,14 @@
 API "1.1"
 
 return etc.translateWords(arg[1], function(w)
-  if #w < 2 then
-    return "bz"
-  else
-    return "b"..("z"):rep(#w-1)
-  end
+  -- used to return "bz", but that looks stupid with results like "BZ"
+  if #w < 2 then return end
+
+  -- skip numbers
+  if w:match("[%d%.]+") then return end
+
+  -- ouch
+  if math.random() < 0.1 then return "*sting*" end
+  
+  return "b"..("z"):rep(#w-1)
 end)
