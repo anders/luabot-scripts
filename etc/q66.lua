00995cd309f5a48f8cd959e28078d81bd04bac28
Anders Bergh <anders1@gmail.com>
2016-03-13 01:48:03 +0000

    Sync.

--- etc/q66.lua
+++ etc/q66.lua
@@ -1,7 +1,13 @@
 t={'%N% reeks',
    'gface',
-   'g f a c e'}
+   'g f a c e',
+   function()
+     return 'I promise not to shitpost for '..math.random(2, 4)..' months'
+   end}
 m=t[math.random(1,#t)]
+if type(m) == "function" then
+  m = m()
+end
 m=m:gsub('%%(%w+)%%', function(s)
   if s == 'N' then return rnick() end
   return '<unknown>'
