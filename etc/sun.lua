e06c019be5dacd89cdbdb76a84026cb1d62e47e9
Anders Bergh <anders1@gmail.com>
2016-04-23 10:48:03 +0000

    Sync.

--- etc/sun.lua
+++ etc/sun.lua
@@ -2,8 +2,6 @@ local Sun = require 'sun'
 
 local who = arg[1] or nick
 
--- local coords = assert(etc.get('location.coords', who), 'use set location')
-
 local coords = etc.get('location.coords', who)
 if not coords then
   local _
@@ -24,16 +22,18 @@ local length, rs, rise, set = Sun.opt_calc(lat, lon, today.year, today.month, to
 
 local ts_rise, offset_rise, tz_code_rise = etc.timezone(who, "%H:%M", rise, true)
 local ts_set, offset_set, tz_code_set = etc.timezone(who, "%H:%M", set, true)
+
+--[[
 print(("Sunrise: %s %s; Sunset: %s %s. Day is %s long."):format(
   ts_rise, tz_code_rise,
   ts_set, tz_code_set,
   etc.duration(math.floor(length * 3600))))
+]]
 
-local ts_rise, offset_rise, tz_code_rise = etc.timezone(who, "%H:%M", rise_past, true)
-local ts_set, offset_set, tz_code_set = etc.timezone(who, "%H:%M", set_past, true)
+local ts_rise2, offset_rise2, tz_code_rise2 = etc.timezone(who, "%H:%M", rise_past, true)
+local ts_set2, offset_set2, tz_code_set2 = etc.timezone(who, "%H:%M", set_past, true)
 
 local diff = (length - length_past) * 3600
--- diff = diff>=0 and ("+"..etc.duration(diff)) or ("-"..etc.duration(math.floor(diff)))
 
 local diff_string
 if diff == 0 then
@@ -44,10 +44,21 @@ else
   diff_string = 'The day is now '..etc.duration(math.floor(diff))..' shorter'
 end
 
+--[[
 print(("A week ago: Sunrise: %s %s; Sunset: %s %s. The day was %s long. %s."):format(
-  ts_rise, tz_code_rise,
-  ts_set, tz_code_set,
+  ts_rise2, tz_code_rise2,
+  ts_set2, tz_code_set2,
   etc.duration(math.floor(length_past * 3600)), diff_string))
+]]
 
--- print(('sunrise: %s %s, sunset: %s %s. day is %s long'):format(
---rise, tzcr, set, tzcs, etc.duration(math.floor(butt.set - butt.rise)))
+-- TODO: transform
+-- 01:34 ABC - 23:00 ABC to 01:34 - 23:00 ABC (same time zone)
+
+etc.printf("Today: %s %s - %s %s (%s). A week ago: %s %s - %s %s (%s). %s.",
+           ts_rise, tz_code_rise,
+           ts_set, tz_code_set,
+           etc.duration(math.floor(length * 3600)),
+           ts_rise2, tz_code_rise2,
+           ts_set2, tz_code_set2,
+           etc.duration(math.floor(length_past * 3600)),
+           diff_string)
