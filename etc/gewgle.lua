e327f09cfbe10f2c6e48502e88387d5f96845ba7
Anders Bergh <anders1@gmail.com>
2016-11-22 11:48:03 +0000

    Sync.

--- etc/gewgle.lua
+++ etc/gewgle.lua
@@ -6,7 +6,11 @@ if query:lower() == 'google' then
   return
 end
 
-local data, err = httpGet('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q='..urlEncode(query))
+
+local key = etc.rot13('NVmnFlPgZTnKY71YpQvGhN98f6irxsBsCl9Efn8')
+local cx = '011942350363630310124:w111ktxbioi'
+
+local data, err = httpGet('https://www.googleapis.com/customsearch/v1?key='..key..'&cx='..cx..'&q='..urlEncode(query))
 if not data then
   return false, err
 end
@@ -16,18 +20,34 @@ if data:sub(1, 1) ~= '{' then
 end
 
 local resp = assert(json.decode(data))
-assert(resp.responseData, resp.responseDetails or 'no response data')
 
-local res = resp.responseData.results
-if res[1] then
-  --print(etc.t(res[1]))
-  -- unescapedUrl
-  -- visibleUrl
-  -- title
-  -- titleNoFormatting
-  -- content
-  
-  print(nick..' * '..etc.html2irc(res[1].titleNoFormatting)..': '..res[1].unescapedUrl)
+--[[
+{
+ "error": {
+  "errors": [
+   {
+    "domain": "usageLimits",
+    "reason": "accessNotConfigured",
+    "message": "Access Not Configured. CustomSearch API has not been used in project 995647563394 before or it is disabled. Enable it by visiting https://console.developers.google.com/apis/api/customsearch/overview?project=995647563394 then retry. If you enabled this API recently, wait a few minutes for the action to propagate to our systems and retry.",
+    "extendedHelp": "https://console.developers.google.com/apis/api/customsearch/overview?project=995647563394"
+   }
+  ],
+  "code": 403,
+  "message": "Access Not Configured. CustomSearch API has not been used in project 995647563394 before or it is disabled. Enable it by visiting https://console.developers.google.com/apis/api/customsearch/overview?project=995647563394 then retry. If you enabled this API recently, wait a few minutes for the action to propagate to our systems and retry."
+ }
+}
+
+ctx.respond(ctx._("({num} of {total}) {title}: {url}").format(
+        title=results[num]["title"],
+        url=results[num]["link"],
+        num=num + 1,
+        total=total
+    ))
+]]
+
+local res = resp.items
+if res and res[1] then
+  print(nick..' * '..res[1].title..': '..res[1].link..' - '..res[1].snippet)
 else
-  return false, 'no results'
+  return false, 'no results'..json.encode(resp.error)
 end
