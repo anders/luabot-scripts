d0dbac191b6a8d90d20038c354fd0c6b52bbd806
Bug Maker <user1@codebust.com>
2017-01-13 19:48:03 +0000

    Sync.

---
+++ etc/_tpbsticker.lua
@@ -0,0 +1,12 @@
+return ({
+  bubbles="BQADBAADPwAD848aA3lTI6kV8z1OAg";
+  julian="BQADBAADQQAD848aAyV-RpqSeYpCAg";
+  ricky="BQADBAADQwAD848aA0pZ87c2NU1_Ag";
+  lahey="BQADBAADRQAD848aA-n6FpcIaKwaAg";
+  randy="BQADBAADRwAD848aA348vsSUvNAuAg";
+  -- ricky2="";
+  phil="BQADBAADUAAD848aA_0oz4oAASUT9AI";
+  -- bubbles2="";
+  baam="BQADBAADVAAD848aAydrA-xEZ_YAAQI";
+  -- ricky3="";
+})[arg[1]]
---
+++ etc/tpb.lua
@@ -0,0 +1,3 @@
+if arg[1] and not sendSticker(etc._tpbsticker(arg[1])) then
+  print(arg[1]:upper() .. "!")
+end
