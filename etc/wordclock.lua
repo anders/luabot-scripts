d716da9c07d0ecdfba80a66784d016756e9f5636
Bug Maker <user1@codebust.com>
2017-01-23 13:48:03 +0000

    Sync.

--- etc/wordclock.lua
+++ etc/wordclock.lua
@@ -4,6 +4,8 @@ return etc.worldclock(...):gsub("(%d%d):(%d%d)", function (h, m)
   local suff = etc.numberToWords(m)
   if suff == "zero" then
     suff = "o'clock"
+  elseif tonumber(m) < 10 then
+    suff = "o'" .. suff
   end
   return etc.numberToWords(h) .. " " .. suff
 end) or ""
