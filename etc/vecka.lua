469d3b8623e5cf04e5678187edf9fbb9450059be
Anders Bergh <anders1@gmail.com>
2017-04-03 14:48:03 +0000

    Sync.

---
+++ etc/vecka.lua
@@ -0,0 +1,1 @@
+return (etc.week(...):gsub("W", "vecka "))
--- etc/week.lua
+++ etc/week.lua
@@ -1,1 +1,7 @@
-return os.date("!%W")
+local who = arg[1] or nick
+local week, ok, zone_short = etc.timezone(who, '%W', nil, true)
+if ok then
+  return "W"..week.." ("..zone_short..")"
+else
+  return "W"..os.date("!%W").." (UTC)"
+end
