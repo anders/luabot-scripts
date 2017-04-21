4a30c8ea8f6c201c631ad7868e581e4099c102a5
Bug Maker <user1@codebust.com>
2017-04-21 19:48:03 +0000

    Sync.

--- util/money_crypto.lua
+++ util/money_crypto.lua
@@ -2,7 +2,7 @@ API "1.1"
 
 local to = arg[1]
 local from = (arg[2] or 'usd'):lower()
-local info = util.coinmarketcap(to)
+local info = assert(util.coinmarketcap(to))
 local prices = info.price
 local val = prices[from]
 return val, info.name
