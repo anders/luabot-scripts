b14fbf211c42bd32a9cbdd4ccd00eac0b03788a5
Bug Maker <user1@codebust.com>
2017-09-10 23:48:03 +0000

    Sync.

--- util/coinmarketcap.lua
+++ util/coinmarketcap.lua
@@ -1,3 +1,15 @@
+local coin = (arg[1] or ""):lower()
+coin = ({eth=ethereum, btc=bitcoin})[coin] or coin
+
 local json = require "json"
-local cc = ...
-return json.decode(httpGet("https://coinmarketcap-nexuist.rhcloud.com/api/" .. cc))
+
+local data, mimeType, charset, status = httpGet("https://api.coinmarketcap.com/v1/ticker/" .. coin .. "/")
+
+assert(data, mimeType)
+
+local dec = json.decode(data)
+
+return tonumber(dec[1].price_usd), dec[1].name
+
+
+
--- util/money_crypto.lua
+++ util/money_crypto.lua
@@ -3,14 +3,14 @@ API "1.1"
 local to = arg[1]
 local from = (arg[2] or 'usd'):lower()
 
---[[
 local info = assert(util.coinmarketcap(to))
 local val = info.price[from]
 local coin_name = info.name
---]]
 
+--[[
 local info = assert(etc.cryptocoincharts(to, from))
 local val = info.price
 local coin_name = info.coin1
+--]]
 
 return val, coin_name
