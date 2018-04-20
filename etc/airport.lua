185d12213e92dd34097409fe757ab585b0fdcc61
Anders Bergh <anders1@gmail.com>
2018-04-20 17:48:02 +0000

    Sync.

--- etc/airport.lua
+++ etc/airport.lua
@@ -1,8 +1,12 @@
 API "1.1"
 
-local resp = httpGet("http://airportcode.riobard.com/airport/"..arg[1].."?fmt=JSON")
+local key = "9ea90ce0-4da6-46eb-a55b-55d9f301b6e8"
+
+local airport = (arg[1] or "NRT"):sub(1, 3)
+
+local resp = httpGet("http://iatacodes.org/api/v6/airports?api_key="..key.."&code="..airport)
 local json = require "json"
---{"code": "FRA", "name": "Frankfurt International Airport", "location": "Frankfurt, Germany"}
 
 local d = assert(json.decode(resp), "probably not a valid IATA code")
-print(d.name.." ("..d.code..") - "..d.location)
+local d1 = d.response[1]
+print(d1.name.." ("..d1.code..")")
