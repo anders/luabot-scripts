3ee4e6cd3d8db629df47f4b17027e42f1cb887d1
Bug Maker <user1@codebust.com>
2020-02-01 00:02:09 +0000

    Sync.

--- etc/API.lua
+++ etc/API.lua
@@ -1,6 +1,9 @@
 -- Usage: API "<number>" on the first line to use a specific API version. See etc.API source for version info.
 
 --[[
+  API 1.2:
+    Automatically returns any strings printed when etc or other module functions are called.
+    Experimental version: this version will stay, but later versions may revert behavior.
   API 1.1:
     Remove getRootFS
     getUserFS updated to use restricted root FS, no longer requires a nickname.
